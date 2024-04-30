3981dd7f01aed3b7d897f3dc2d1f3ae19149f945
Murali Krishna Mallela <mmallela9@gmail.com>
2024-04-30 20:30:18 +0530

    added quick sort tracing

---
+++ quick_sort.py
@@ -0,0 +1,89 @@
+
+
+# Swapping
+def swap(arr, i, j):
+    # print("\n","~"*50,"\n")
+    # print("Before swapping list : " , *arr)
+    print(f"\n{arr[i]} and {arr[j]} are swapped.")
+    arr[i], arr[j] = arr[j], arr[i]
+    # print("After swapping list : ", *arr)
+    # print("\n","~"*50,"\n")
+    
+
+# Performing Partitioning   
+def partition(arr, low, high):
+    print("\n","."*70,"\n")
+    print("Performing partition on list = ",*arr[low:high+1],f" with low = {low} and high = {high}")
+    pivot = arr[low]
+    left = low + 1 
+    right = high
+    done = False
+    while not done:
+        print(f"\nPivot = {pivot}")
+        print(f"\nleft = {left} and right=  {right}")
+        while left <= right and arr[left] <= pivot:
+            print()
+            print(f"{arr[left]} <= {pivot}   \n Increasing the left i.e., {left+1}")
+            left = left + 1
+        print(f"\nStopping the left at {left} because arr[left] is not <= pivot ")
+        while arr[right] >= pivot and right >= left:
+            print()
+            print(f"{arr[right]} >= {pivot}   \n Decreasing the right i.e., {right-1}")
+            right = right -1
+        print(f"\nStopping the right at {right} because arr[right] not >= pivot")
+            
+        print(f"\nleft = {left} and right=  {right}")
+        if right <= left:
+            done= True
+        else:
+            print()
+            print(f"left < right i.e., {left} <= {right}   \n Swapping arr[right] and arr[left] i.e., {arr[right]} and {arr[left]}")
+            print("\nBefore swapping list : " , *arr[low:high+1])
+            swap(arr, left, right)
+            print("\nAfter swapping list : ", *arr[low:high+1])
+    print()
+    print(f"left >= right i.e., {left} >= {right}   \n Swapping  pivot and arr[right] i.e., {pivot} and {arr[right]}")
+    print("\nBefore swapping list : " , *arr[low:high+1])
+    swap(arr, low, right)
+    print("\nAfter swapping list : ", *arr[low:high+1])
+    # print(f"Returning the pivot position i.e., {right}")
+    print("\n","."*70)
+    return right
+
+# Performing Quick sort
+def quick_sort(arr, low, high):
+    
+    
+    if low < high:
+        print("\n","*"*70,"\n")
+        print("Performing quick sort on list = ",*arr[low:high+1],f" with low = {low} and high = {high}")
+        # print("\n","+"*50,"\n")
+        # print(f"\nlow ({low}) < high ({high})\n")
+        print("\nFinding the pivot  of list = ",*arr[low:high+1],f" with low = {low},and high = {high}")
+        pivot = partition(arr, low, high)
+        print()
+        print(f"The pivot is {arr[pivot]} at position {pivot} in the list ", *arr)
+        
+        # print("\n","."*50,"\n")
+        # print(f"Performing quick sort with list = {arr} , low = {low} and high = {pivot-1}")
+        quick_sort(arr, low, pivot-1)
+        # print("\n","."*50,"\n")
+        # print(f"Performing quick sort with list = {arr} , low = {pivot+1} and high = {high}")
+        quick_sort(arr, pivot+1, high)
+        # print("\n","+"*50,"\n")
+    
+
+print("\n","-"*70,"\n")
+# Taking the list from the user
+# print("\n","."*50,"\n")
+size = int(input("Enter the size of the list: "))
+my_list = []
+for i in range(size):
+    my_list.append(int(input(f"Enter the element at position {i + 1}: ")))
+print("Original List:", *my_list)
+quick_sort(my_list, 0, len(my_list)-1)
+print("\n","-"*70,"\n")
+
+print("Finally, \nSorted List:", *my_list)
+
+print("\n","*"*70,"\n\t\t\tMURALI KRISHNA MALLELA\n","*"*70)
